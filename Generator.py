d19f29e845925d6758627085e3245b56e74ac86b
Aya <Aya@Ayas-MacBook-Pro.local>
2025-08-12 23:26:15 +0300

    Add a sec method that make a method with ch and number

--- Generator.py
+++ Generator.py
@@ -1,5 +1,6 @@
 import math as ma
 import random as ra
+import string 
 
 # password ruls 
 #    1- must at least by 9 ch 
@@ -8,13 +9,26 @@ import random as ra
 class PassWordGenerator :
     
     # Easy password with Numbers only 
-    def EasyPassword ():
+    def EasyPassword (self):
      password = ""
      for ch in range(9):
         password += str(ra.randint(0 , 9))
      return password
 
-    # password with 
+    # password with Nch and nubmers 
+    def MidPassword(self):
+       password = ""
+       for ch in range(9):
+        if ch%2 == 0 :
+            password +=ra.choice(string.digits)
+        else :    
+            password +=ra.choice(string.ascii_letters)
+       return password
+      
+if __name__ == "__main__":
+   NewPassword = PassWordGenerator()
+   print(NewPassword.EasyPassword())
+   print(NewPassword.MidPassword())
 
 
 
