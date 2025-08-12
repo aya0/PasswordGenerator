a00c7da12098223b090b38390a11bf59af4322e5
Aya <Aya@Ayas-MacBook-Pro.local>
2025-08-13 00:11:56 +0300

    Add HighLevelPassword method to make password with letters , punctiontion , number rondmly

--- Generator.py
+++ Generator.py
@@ -1,4 +1,3 @@
-import math as ma
 import random as ra
 import string 
 
@@ -15,20 +14,62 @@ class PassWordGenerator :
         password += str(ra.randint(0 , 9))
      return password
 
-    # password with Nch and nubmers 
+    # password with Nch and nubmers make it somewho difecalte
     def MidPassword(self):
        password = ""
        for ch in range(9):
+        ch = ch * ra.randint(0 , 9)
         if ch%2 == 0 :
             password +=ra.choice(string.digits)
-        else :    
+        else : 
             password +=ra.choice(string.ascii_letters)
+
        return password
+    
+
+        
+    def HighLevelPassword(self):
+        password = ""
+        for _ in range(9):
+           val = ra.randint(0, 72)
+           if val % 2 == 0:
+               password += ra.choice(string.digits)
+           elif val % 3 == 0:
+               password += ra.choice(string.ascii_letters)
+           else:
+               password += ra.choice(string.punctuation)
+
+        if self.__cheakPasswordValid__(password):
+           return password
+        else:
+        # maybe retry or return None
+           return None
+
+    
+     # cheak if the password at least have one punctuation and one letter 
+    def __cheakPasswordValid__(self , password):
+        has_letter = False
+        has_punc = False
+
+        for ch in password :
+          if ch.isalpha():
+            has_letter =True
+          elif ch in string.punctuation:
+            has_punc = True 
+
+        if has_letter and  has_punc :
+           return True 
+        
+        return False     
+                  
+       
+       
       
 if __name__ == "__main__":
    NewPassword = PassWordGenerator()
    print(NewPassword.EasyPassword())
    print(NewPassword.MidPassword())
+   print(NewPassword.HighLevelPassword())
 
 
 
